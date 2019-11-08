c516d0e3536812bd4787d3203441b235bab89b94
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2019-11-08 11:36:54 -0500

    Set up project dependencies & index.js with needed dependencies, project start

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,12 @@
+export const initialState = {};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    // case '':
+    //     return {
+
+    //     }
+    default:
+      return state;
+  }
+};
